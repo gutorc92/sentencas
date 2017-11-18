fe148e4b6ab094d173c37c6470d6afe2d29ddbf4
Gustavo Coelho <gutorc@hotmail.com>
2017-11-18 17:22:25 -0200

    List process
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ generate_list_process.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import os
+import platform
+import urllib.request
+import zipfile
+from settings import Settings
+from downloadsentences import DownloadSetence
+
+
+
+
+
+if __name__ == "__main__":
+    s = Settings()
+    s.extract_settings()
+    list_files = os.listdir(os.path.join(s.path, "numero_processos"))
+    process = set()
+    for file_path in list_files:
+        with open(os.path.join(s.path, "numero_processos", file_path), "r") as f:
+            for line in f.readlines():
+                process.add(line.replace("\n", ""))
+
+    print(len(process))
+
