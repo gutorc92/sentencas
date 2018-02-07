059833d083e92bdc75f1c280a52b2d4899d65a48
gutorc92 <gutorc@hotmail.com>
2018-02-07 11:01:25 -0200

    Backup
    Signed-off-by: gutorc92 <gutorc@hotmail.com>

--- generate_list_process.py
+++ generate_list_process.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
 import platform
+import sys
+from datetime import datetime
 import urllib.request
 import zipfile
 from settings import Settings
-from downloadsentences import DownloadSetence
 
 
 
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1 and str(sys.argv[1]) == 'backup':
+        save_files = True
+    else:
+        save_files = False
     s = Settings()
     list_files = os.listdir(os.path.join(s.path, "numero_processos"))
     process = set()
@@ -19,5 +24,11 @@ if __name__ == "__main__":
             for line in f.readlines():
                 process.add(line.replace("\n", ""))
 
-    print(len(process))
 
+    print(len(process))
+    if save_files:
+        file_name = "backup_" + datetime.now().strftime("%d%m%Y_%H_%M")
+        with open(os.path.join(s.path, "numero_processos", file_name), "w") as f:
+            for number in process:
+                f.write(number)
+                f.write("\n")
